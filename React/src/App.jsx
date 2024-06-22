1c936e9d494ad4c0962b37d436dc480364ac6988
redheadphones911911 <74419059+redheadphones911911@users.noreply.github.com>
2024-06-22 22:34:35 +0000

    u

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -5,12 +5,20 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const inputHandle = () => {
+    let input = document.getElementById('txt')
+    let print = input.value
+    let para = document.getElementById('parag')
+    let outside = para.textContent(print)
+     return outside
+  }
 
   return(
  <div>
   <p>Type your name:</p>
-  <input type='text'></input> 
-<button>Submit</button>
+  <input type='text' id='txt'></input> 
+<button onClick={inputHandle}>Submit</button>
+<p id='parag'></p>
  </div>
   )
 }
