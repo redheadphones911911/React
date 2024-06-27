db7132ae6ae7ead82059bdfa2f9f96bbc23bdd3f
redheadphones911911 <74419059+redheadphones911911@users.noreply.github.com>
2024-06-27 23:23:32 +0000

    u

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -3,6 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {}
+function App() {
+    const [value , setValue] = useState("");
+    return(
+        <div className='container'>
+            <input type='text'/>
+            <div className='wrapper'></div>
+            <div>
+            <input type='button' value={7} />
+            <input type='button' value={8} />
+            <input type='button' value={9}/>
+            <input type='button' value={DEL} />
+            <input type='button' value={AC} />
+            </div>
+        </div>
+    )
+}
 
 export default App
