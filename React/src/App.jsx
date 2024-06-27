7167c8b83dbc733dd000fe90012bf3a02bb436e5
redheadphones911911 <74419059+redheadphones911911@users.noreply.github.com>
2024-06-27 23:36:30 +0000

    u

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -8,13 +8,35 @@ function App() {
     return(
         <div className='container'>
             <input type='text'/>
-            <div className='wrapper'></div>
+            <div className='wrapper'>
             <div>
             <input type='button' value={7} />
             <input type='button' value={8} />
             <input type='button' value={9}/>
-            <input type='button' value={DEL} />
-            <input type='button' value={AC} />
+            <input type='button' value={"DEL"} />
+            <input type='button' value={"AC"} />
+            </div>
+            <div>
+            <input type='button' value={4} />
+            <input type='button' value={5}/>
+            <input type='button' value={6}/>
+            <input type='button' value={"×"}/>
+            <input type='button' value={"÷"}/>
+            </div>
+            <div>
+                <input type='button' value={1}/>
+                <input type='button' value={2}/>
+                <input type='button' value={3}/>
+                <input type='button' value={"+"}/>
+                <input type='button' value={"-"}/>
+            </div>
+            <div>
+                <input type='button' value={0}/>
+                <input type='button' value={"."}/>
+                <input type='button' value={"×10"}/>
+                <input type='button' value={"Ans"}/>
+                <input type='button' value={"="}/>
+            </div>
             </div>
         </div>
     )
