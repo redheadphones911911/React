5a90a3952151230c243b6380af1f4f93e83fd727
redheadphones911911 <74419059+redheadphones911911@users.noreply.github.com>
2024-06-28 12:39:11 +0000

    u

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -13,29 +13,29 @@ function App() {
             <input type='button' value={"7"} onClick={e  => { setValue(value + e.target.value)}}/>
             <input type='button' value={"8"} onClick={e => {setValue(value + e.target.value)}}/>
             <input type='button' value={"9"} onClick={e => {setValue(value + e.target.value)}}/>
-            <input type='button' value={"DEL"} />
-            <input type='button' value={"AC"} onClick={setValue("")} />
+            <input type='button' value={"DEL"} onClick={e => setValue(value.slice(0,-1))}/>
+            <input type='button' value={"AC"} onClick={e => {setValue("")}}/>
             </div>
             <div>
             <input type='button' value={"4"} onClick={e => {setValue(value + e.target.value)}}/>
             <input type='button' value={"5"} onClick={e => {setValue(value + e.target.value)}}/>
             <input type='button' value={"6"} onClick={e => {setValue(value + e.target.value)}}/>
-            <input type='button' value={"×"} onClick={e => {setValue(value + e.target.value)}}/>
-            <input type='button' value={"÷"} onClick={e => {setValue(value + e.target.value)}}/>
+            <input type='button' value={"*"} onClick={e =>{setValue(value + e.target.value)}}/>
+            <input type='button' value={"/"} onClick={e => {setValue(value + e.target.value)}}/>
             </div>
             <div>
-                <input type='button' value={1} onClick={e => {setValue(value + e.target.value)}}/>
-                <input type='button' value={2} onClick={e => {setValue(value + e.target.value)}}/>
-                <input type='button' value={3} onClick={e => {setValue(value + e.target.value)}}/>
+                <input type='button' value={"1"} onClick={e => {setValue(value + e.target.value)}}/>
+                <input type='button' value={"2"} onClick={e =>{setValue(value + e.target.value)}}/>
+                <input type='button' value={"3"} onClick={e => {setValue(value + e.target.value)}}/>
                 <input type='button' value={"+"} onClick={e => {setValue(value + e.target.value)}}/>
                 <input type='button' value={"-"} onClick={e => {setValue(value + e.target.value)}}/>
             </div>
             <div>
                 <input type='button' value={"0"} onClick={e => {setValue(value + e.target.value)}}/>
                 <input type='button' value={"."} onClick={e => {setValue(value + e.target.value)}}/>
-                <input type='button' value={"10"} onClick={e => {setValue(value + e.target.value)}}/>
-                <input type='button' value={"Ans"}/>
-                <input type='button' value={"="}/>
+                <input type='button' value={"*10"} onClick={e => {setValue(value + e.target.value)}}/>
+                <input type='button' value={"Ans"} onClick={e => {setValue(value)}}/>
+                <input type='button' value={"="} onClick={e => setValue(eval(value))}/>
             </div>
             </div>
         </div>
